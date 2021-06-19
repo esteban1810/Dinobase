d39c4edfc21676ce5fc2542979917463cd4e622f
esteban1810 <estebansevillapg@gmail.com>
2021-06-19 04:52:51 -0500

    base de datos

--- src/BD/Conexion.java
+++ src/BD/Conexion.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 //Estoy haciendo pruebas
 public class Conexion {
     private final String usuario="postgres";
-    private final String pass="1234";
-    private final String BD="dinobase";
+    private final String pass="esteban3";
+    private final String BD="dino";
     private final String host="localhost";
     private final String port="5432";
     private Connection con=null;
